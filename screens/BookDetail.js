b7b87cfa901c362a5bc2236bd91d25978cdbe679
DucSkii <ducvietdao@live.co.uk>
2021-08-07 18:38:46 +0100

    working on bookdetaisl page

--- screens/BookDetail.js
+++ screens/BookDetail.js
@@ -11,10 +11,32 @@ const BookDetail = () => {
         source={books[0].image}
         blurRadius={100}
       >
-        <View style={{ height: '65%' }}>
-          <Text style={{ color: 'white' }}>asdsad</Text>
+        <View style={{ height: '100%' }}>
+          <View style={styles.detailContainer}>
+            <Image style={{ height: '50%', width: '35%' }} source={books[0].image} />
+            <View style={{ marginTop: 15 }}>
+              <Text style={{ color: 'white', fontSize: 18, fontWeight: 'bold', textAlign: 'center' }}>{books[0].title}</Text>
+              <Text style={{ color: 'white', fontSize: 14, textAlign: 'center', marginTop: 5, }}>{books[0].author}</Text>
+            </View>
+            <View style={styles.detailBox}>
+              <View style={styles.detailBoxBackground}>
+                <View style={[styles.boxSection, { borderTopLeftRadius: 15, borderBottomLeftRadius: 15, }]}>
+
+                </View>
+                <View style={{ height: '40%', backgroundColor: 'white', opacity: 0.15, width: 1, }} />
+                <View style={styles.boxSection}>
+
+                </View>
+                <View style={{ height: '40%', backgroundColor: 'white', opacity: 0.15, width: 1, }} />
+                <View style={[styles.boxSection, { borderTopRightRadius: 15, borderBottomRightRadius: 15, }]}>
+
+                </View>
+              </View>
+            </View>
+          </View>
         </View>
       </ImageBackground>
+      <Text style={{ color: 'white' }}>asdsad</Text>
     </View>
   )
 }
@@ -25,8 +47,31 @@ const styles = StyleSheet.create({
   },
   image: {
     flex: 1,
-    height: '65%',
     width: '100%',
+    minHeight: '65%',
+    maxHeight: '65%',
+  },
+  detailContainer: {
+    marginTop: '25%',
+    height: '80%',
+    alignItems: 'center',
+  },
+  detailBox: {
+    flex: 1,
+    width: '100%',
+    padding: 25,
+  },
+  detailBoxBackground: {
+    flex: 1,
+    flexDirection: 'row',
+    width: '100%',
+    backgroundColor: 'rgba(0, 0, 0, 0.15)',
+    alignItems: 'center',
+    borderRadius: 15,
+  },
+  boxSection: {
+    flex: 1,
+    height: '100%',
   },
 })
 
